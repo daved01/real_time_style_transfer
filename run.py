883b2416aafe1e4ff500d34506d72994e0afff2c
daved01 <david.kirchhoff@icloud.com>
2021-12-30 20:38:31 +0100

    Added subfolder for generated images

--- run.py
+++ run.py
@@ -27,7 +27,7 @@ def generate_image(content_image_path, generated_image_path, model_weights_path,
     model_weights_name = raw[0]
 
     try:
-        image = keras.preprocessing.image.load_img(content_image_path + "/" + image_name + ".jpg")
+        image = keras.preprocessing.image.load_img("".join([content_image_path, "/" + image_name, ".jpg"]))
     except FileNotFoundError:
         print("Error! Did not find image. Are your image name and content_image_path in configuration.yaml correct?")
         exit()
@@ -53,9 +53,7 @@ def generate_image(content_image_path, generated_image_path, model_weights_path,
         [print(name) for name in supported_network_architectures]
         exit()
 
-
     try:
-        print(model_weights_path + "/" + model_name + ".h5")
         network.load_weights(model_weights_path + "/" + model_name + ".h5")
     except BaseException:
         print("Error! Did not find model. Are your model name and the model_weights_path in configuration.yaml correct?")
@@ -72,7 +70,7 @@ def generate_image(content_image_path, generated_image_path, model_weights_path,
     # <networkArchitecure>_<dataSet>_<style_image>_batchsize<batchSize>_epochs<numEpochs>.h5
     img = ImagePIL.fromarray(np.uint8(generated_image)).convert('RGB')
     style_image_name, epochs, batch_size = raw[2], raw[4].split(".")[0], raw[3]
-    filename = image_name + "_" + style_image_name + "_" + epochs + "_" + batch_size
+    filename = image_name + "_" + style_image_name + "_" + batch_size + "_" + epochs
     img.save(generated_image_path + "/" + filename + ".png")
     
 
@@ -84,15 +82,13 @@ if __name__ == "__main__":
     parser.add_argument('--allModels', default=None, help="Provide the name of a model folder. Iterates through all models.")
     parser.add_argument('--image', default=None, help="Provide the name of a content image in the folder data/content.")
     
-
     args = parser.parse_args()
     model_name = args.model
-    model_folder = args.allModels
+    model_folder_name = args.allModels
     image_name = args.image
     
-
     # Check inputs
-    if model_name != None and model_folder != None:
+    if model_name != None and model_folder_name != None:
         print("Error! Provide either --model or --allModels.")
         exit()
 
@@ -100,14 +96,24 @@ if __name__ == "__main__":
     print("\n==============================\nTransforming the image...\n")
     content_image_path, generated_image_path, model_weights_path = utils.get_configuration_paths()
 
-    # TODO: Create folder for output.
+    # Update model folder name if single model is given.
+    if model_name != None:
+        model_folder_name = "_".join(model_name.split("_")[:-1])
+
+    # Update paths with model name for subfolder.
+    generated_image_path = "".join([generated_image_path, "/", model_folder_name])
+    model_weights_path = "".join([model_weights_path, "/", model_folder_name])
+    
+    # Create folder for generated images.
+    try:
+        os.mkdir(generated_image_path)
+    except FileExistsError:
+        pass
 
     # Run either single model or multiple models.
     if model_name != None:
-        model_weights_path += "/" + "_".join(model_name.split("_")[:-1])
         generate_image(content_image_path, generated_image_path, model_weights_path, image_name, model_name)
     else:
-        model_weights_path += "/" + model_folder
         # Get all model names in folder and iterate over them.
         model_names = [file.split(".")[0] for file in os.listdir(model_weights_path) if file.endswith(".h5")]
         print("Found {:.0f} models!".format(len(model_names)))
